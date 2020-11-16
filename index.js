6875530aaadf856beaaca16fb211be61e8cca4c3
Henry Myers <hankhowland@gmail.com>
2020-11-16 15:27:59 -0600

    fixed port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 const mongo = require('mongodb');
 
 const app = express();
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 //for post forms
 app.use(bodyParser.json());
